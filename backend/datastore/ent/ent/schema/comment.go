6fcc29534b9c4572eb0747735a0b180df2b6e5f7
Marcus Sanchez <marcussanchez031@gmail.com>
2024-04-27 15:44:40 -0400

    create O2M comment relationship Comment <-- Comment (replies)

--- backend/datastore/ent/ent/schema/comment.go
+++ backend/datastore/ent/ent/schema/comment.go
@@ -19,7 +19,7 @@ func (Comment) Fields() []ent.Field {
 		field.UUID("id", uuid.UUID{}).Unique().Immutable(),
 		field.UUID("user_id", uuid.UUID{}).Nillable().Immutable(),
 		field.UUID("media_id", uuid.UUID{}).Immutable(),
-		field.UUID("replying_to", uuid.UUID{}).Nillable().Optional().Immutable(),
+		field.UUID("replying_to_id", uuid.UUID{}).Nillable().Optional().Immutable(),
 		field.String("content"),
 		field.Time("created_at").Immutable(),
 		field.Time("updated_at").Nillable().Optional(),
@@ -33,6 +33,9 @@ func (Comment) Edges() []ent.Edge {
 		edge.From("user", User.Type).Ref("comments").Field("user_id").Unique().Required().Immutable(),
 		// O2M Comment <-- Like
 		edge.To("likes", Like.Type).Annotations(entsql.OnDelete(entsql.Cascade)),
+		// O2M Comment <-- Comment (Replies)
+		edge.To("replies", Comment.Type).From("replying_to").Field("replying_to_id").
+			Immutable().Unique().Annotations(entsql.OnDelete(entsql.Cascade)),
 		// O2M Media <-- Comment
 		edge.From("media", Media.Type).Ref("comments").Field("media_id").Unique().Required().Immutable(),
 	}
